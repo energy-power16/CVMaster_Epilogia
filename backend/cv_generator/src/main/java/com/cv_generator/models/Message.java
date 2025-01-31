c8d6b0da8a045161d2e08e4c1bce5c4e06b8f363
ners1us <66124861+ners1us@users.noreply.github.com>
2025-01-31 14:23:42 +0300

    refactor: change json message structure

--- backend/cv_generator/src/main/java/com/cv_generator/models/Message.java
+++ backend/cv_generator/src/main/java/com/cv_generator/models/Message.java
@@ -1,7 +1,16 @@
 package com.cv_generator.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public class Message extends BaseContent {
+    @JsonCreator
     public Message(String content) {
         this.setContent(content);
+    }
+
+    @JsonValue
+    public String getContent() {
+        return super.getContent();
     }
 }
